1010a8b98bcf7070369d6981bdcf57f060a94e07
Olena BONDARENKO <olbondar@student.unit.ua>
2018-06-02 14:00:59 +0300

    do put_pixel.c

--- src/set_julia.c
+++ src/set_julia.c
@@ -24,8 +24,8 @@ void set_julia(t_map *map)
 	}
 	if (map->curr_iteration == map->max_iteration)
 		//put_pxl_to_image(map, map->x, map->y);
-		puts("do put_pxl_to_image");
+		puts("do put_pixel");
 	else
 		//put_pxl_to_image2(map, map->x, map->y);
-		puts("do put_pxl_to_image");
+		puts("do put_pixel_2");
 }
